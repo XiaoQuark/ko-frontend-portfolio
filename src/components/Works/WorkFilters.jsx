f39f942b8fc38205dc8f2e4e37f0507986ccfb98
XiaoQuark <kris.oldrini@gmail.com>
2024-07-31 09:31:32 +0100

    implement projects filtering options

---
+++ src/components/Works/WorkFilters.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+export function WorkFilters({ categories, setFilter }) {
+	const [active, setActive] = useState(0);
+
+	const handleClick = (e, index) => {
+		setFilter({ name: e.target.textContent });
+		setActive(index);
+	};
+
+	return (
+		<ul className="works-filters">
+			{categories.map((item, index) => {
+				return (
+					<li
+						onClick={(e) => {
+							handleClick(e, index);
+						}}
+						// className="category"
+						className={`${
+							active === index ? "active-category" : ""
+						} category`}
+						key={index}
+					>
+						{item.name}
+					</li>
+				);
+			})}
+		</ul>
+	);
+}
--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -1,44 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Project } from "./Project";
 import "./Works.css";
-import projects from "../../data/projects.json";
+import projectsData from "../../data/projects.json";
 import categories from "../../data/categories.json";
 import { Modal } from "./Modal";
+import { WorkFilters } from "./WorkFilters";
 
 export function Works() {
-	// const [modalActive, setModalActive] = useState(false);
-	const [projectFilter, setProjectFilter] = useState("all");
+	const [filter, setFilter] = useState({ name: "all" });
+	const [projects, setProjects] = useState([]);
+
+	useEffect(() => {
+		if (filter.name === "all") {
+			setProjects(projectsData);
+		} else {
+			const filteredProjects = projectsData.filter((project) => {
+				return project.category.some((item) => item === filter.name);
+			});
+			console.log(filteredProjects);
+			setProjects(filteredProjects);
+		}
+	}, [filter]);
 
 	return (
 		<section className="works section" id="works">
 			<h2 className="section-title">Portfolio</h2>
 			<span className="section-subtitle">My most recent projects</span>
-			<ul className="works-filters">
-				{categories.map((item, index) => {
-					return (
-						<li className="category" key={index}>
-							{item.name}
-						</li>
-					);
-				})}
-			</ul>
-			<div className="works-container container grid">
-				{projects.map((project) => {
-					return <Project project={project} key={project.id} />;
-				})}
+			<div>
+				<WorkFilters categories={categories} setFilter={setFilter} />
+				<div className="works-container container grid">
+					{projects.map((project) => {
+						return <Project project={project} key={project.id} />;
+					})}
+				</div>
 			</div>
-			{/* <div className="works-modal">
-				{projects.map((project) => {
-					return (
-						<Modal
-							project={project}
-							modalActive={modalActive}
-							setModalActive={setModalActive}
-							key={project.id}
-						/>
-					);
-				})}
-			</div> */}
 		</section>
 	);
 }
