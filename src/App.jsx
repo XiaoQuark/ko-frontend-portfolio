c90f9d304a5b39c96d2620a0b8f50b75f33ec6b7
XiaoQuark <kris.oldrini@gmail.com>
2024-08-01 11:28:31 +0100

    create contacts section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { About } from "./components/About/About";
+import { Contacts } from "./components/Contacts/Contacts";
 import { Header } from "./components/Header/Header";
 import { Hero } from "./components/Hero/Hero";
 import { Skills } from "./components/Skills/Skills";
@@ -14,6 +15,7 @@ function App() {
 				<About />
 				<Skills />
 				<Works />
+				<Contacts />
 			</main>
 		</>
 	);
---
+++ src/components/Contacts/Contacts.jsx
@@ -0,0 +1,137 @@
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import { UilArrowRight } from "@iconscout/react-unicons";
+import "./Contacts.css";
+
+export function Contacts() {
+	const form = useRef();
+
+	const sendEmail = (e) => {
+		e.preventDefault();
+
+		emailjs.sendForm("service_p68nyps", "template_lt39ey9", form.current, {
+			publicKey: "YoRYUiKa30VPW9KEW",
+		});
+		e.target.reset().then(
+			() => {
+				console.log("SUCCESS!");
+			},
+			(error) => {
+				console.log("FAILED...", error.text);
+			}
+		);
+	};
+	return (
+		<section className="contacts section" id="contacts">
+			<h2 className="section-title">Contacts</h2>
+			<span className="section-subtitle">Get in Touch</span>
+			<div className="contact-container container grid">
+				<div className="contact-content">
+					<h3 className="contact-title">Ways to contact me</h3>
+
+					<div className="contact-info">
+						<div className="contact-card">
+							<i class="bx bx-mail-send contact-card-icon"></i>
+							<h4 className="contact-card-title">Email</h4>
+							<span className="contact-card-data">
+								kris.oldrini@gmail.com
+							</span>
+							<a
+								href="mailto:kris.oldrini@gmail.com"
+								className="contact-button"
+							>
+								Send
+								<UilArrowRight className="contact-button-icon" />
+							</a>
+						</div>
+						<div className="contact-card">
+							<i class="bx bxl-whatsapp contact-card-icon"></i>
+							<h4 className="contact-card-title">WhatsApp</h4>
+							<span className="contact-card-data">
+								+44 (0) 7514760946
+							</span>
+							<a
+								href="https://api.whatsapp.com/send?prone"
+								className="contact-button"
+							>
+								Send
+								<UilArrowRight className="contact-button-icon" />
+							</a>
+						</div>
+					</div>
+				</div>
+
+				<div className="contact-content">
+					<h3 className="contact-title">Share your thoughts</h3>
+					<form
+						ref={form}
+						onSubmit={sendEmail}
+						className="contact-form"
+					>
+						<div className="contact-form-div">
+							<label htmlFor="" className="contact-form-tag">
+								Name
+							</label>
+							<input
+								type="text"
+								name="name"
+								className="contact-form-input"
+								placeholder="Jessica Fletcher"
+							/>
+						</div>
+						<div className="contact-form-div">
+							<label htmlFor="" className="contact-form-tag">
+								Email
+							</label>
+							<input
+								type="email"
+								name="email"
+								className="contact-form-input"
+								placeholder="mrsfletcher@email.com"
+							/>
+						</div>
+						<div className="contact-form-div contact-form-area">
+							<label htmlFor="" className="contact-form-tag">
+								Message
+							</label>
+							<textarea
+								name="message"
+								cols="30"
+								rows="10"
+								className="contact-form-input"
+								placeholder="Hi, ..."
+							></textarea>
+						</div>
+						<button className="button button-flex">
+							Send Message!
+							<svg
+								className="button-icon"
+								xmlns:xlink="http://www.w3.org/1999/xlink"
+								width="24"
+								height="24"
+								viewBox="0 0 24 24"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									d="M18.8951 3.61502C19.7248 3.37794 20.492 4.1451 20.2549 4.97489L16.2553 18.9736C15.8267 20.4736 13.823 20.7554 12.9973 19.4317L10.1999 14.947C9.87715 14.4296 9.44039 13.9928 8.92298 13.6701L4.43823 10.8726C3.11455 10.047 3.39632 8.04323 4.89636 7.61465L18.8951 3.61502Z"
+									stroke="var(--container-colour)"
+									stroke-width="1.5"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								/>
+								<path
+									d="M10.1924 13.6777L13.7279 10.1422"
+									stroke="var(--container-colour)"
+									stroke-width="1.5"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								/>
+							</svg>
+						</button>
+					</form>
+				</div>
+			</div>
+		</section>
+	);
+}
